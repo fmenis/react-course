74c68f6967c60be35352460822d09dae2d12e07b
Filippo Menis <filippomeniswork@gmail.com>
2025-11-02 15:05:34 +0100

    winner algorithm

--- 02-tic-tac-toe-starting-project/src/App.jsx
+++ 02-tic-tac-toe-starting-project/src/App.jsx
@@ -4,9 +4,45 @@ import GameBoard from "./components/GameBoard.jsx";
 import Player from "./components/Player.jsx";
 import Log from "./components/Log.jsx";
 
+import { WINNING_COMBINATIONS } from "./winning-combinations.js";
+
+const gameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
+
   const activePlayerSymbol = deriveActivePlayerSymbol(gameTurns);
+
+  // populate gameBoard through turn logs
+  for (const turn of gameTurns) {
+    const { activePlayerSymbol, square } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = activePlayerSymbol;
+  }
+
+  let winner = null;
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol =
+      gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol =
+      gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol =
+      gameBoard[combination[2].row][combination[2].column];
+
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol;
+    }
+  }
 
   function handleSelectSquare(rowIndex, colIndex) {
     setGameTurns((prevTurns) => {
@@ -39,7 +75,8 @@ function App() {
             isActive={activePlayerSymbol === "O"}
           />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+        {winner && <p>You won {winner}</p>}
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
     </main>
@@ -48,6 +85,7 @@ function App() {
 
 function deriveActivePlayerSymbol(gameTurns) {
   let activePlayerSymbol = "X";
+
   if (gameTurns.length && gameTurns[0].activePlayerSymbol === "X") {
     activePlayerSymbol = "O";
   }

6f5475136e0b9fd55867e43226218accd0a2b575
Filippo Menis <filippomeniswork@gmail.com>
2025-11-13 11:50:50 +0100

    enable strict mode

--- 04-login-example/src/main.jsx
+++ 04-login-example/src/main.jsx
@@ -1,6 +1,12 @@
-import ReactDOM from 'react-dom/client';
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
 
-import App from './App.jsx';
-import './index.css';
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // every component will be executed twice (not in production)
+  <StrictMode>
+    <App />
+  </StrictMode>
+);

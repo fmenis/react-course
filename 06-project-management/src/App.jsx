c68b4abfe29f08d40ac1098cf1003102041cec81
Filippo Menis <filippomeniswork@gmail.com>
2025-11-14 16:43:52 +0100

    first components

--- 06-project-management/src/App.jsx
+++ 06-project-management/src/App.jsx
@@ -1,8 +1,14 @@
+import ProjectSidebar from "./components/ProjectSidebar.jsx";
+import NewProject from "./components/NewProject.jsx";
+
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8 ">
+      <ProjectSidebar
+        projects={[{ name: "Learning React" }, { name: "Mastering React" }]}
+      />
+      <NewProject />
+    </main>
   );
 }
 
